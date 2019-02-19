338c5ac9d887c1d55e93cbff3404560eca0bb777
rmejia-tajc <rmejia.tajc@gmail.com>
2019-02-19 14:07:51 -0700

    create post endpoint

--- index.js
+++ index.js
@@ -19,6 +19,31 @@ server.use(helmet());
 
 // endpoints here
 
+// error responses
+const errors = {
+  '19': 'Another record with that value exists',
+};
+
+// create zoo
+server.post('/api/zoos', async (req, res) => {
+  if (!req.body.name) {
+    res.status(400).json({ message : 'Please enter a value for this entry'});
+  } else {
+      try {
+        const [id] = await db('zoos').insert(req.body);
+
+        const role = await db('zoos')
+          .where({ id })
+          .first();
+
+        res.status(201).json(id);
+      } catch (error) {
+        const message = errors[error.errno] || 'We ran into an error creating that entry';
+        res.status(500).json({ message, error });
+      }
+    }
+});
+
 
 
 
